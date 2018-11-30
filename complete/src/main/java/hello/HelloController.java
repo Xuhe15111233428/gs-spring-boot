06321cb0063c94449a6999e4d9ff87c1d4de704a
xuhe <768073583@qq.com>
2018-11-30 22:21:41 +0800

    first spring boot commit

--- complete/src/main/java/hello/HelloController.java
+++ complete/src/main/java/hello/HelloController.java
@@ -3,6 +3,9 @@ package hello;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Map;
+import java.util.HashMap;
+
 @RestController
 public class HelloController {
     
@@ -10,5 +13,15 @@ public class HelloController {
     public String index() {
         return "Greetings from Spring Boot!";
     }
-    
+
+    @RequestMapping("/whoareyou")
+    public Map<String,String> who(){
+
+        Map<String,String> map = new HashMap<>();
+        map.put("name","许鹤");
+        map.put("sex","男");
+        map.put("blog","xh_heyingweiwei");
+        return map;
+    }
+
 }
